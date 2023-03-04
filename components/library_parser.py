56c965d2dea7a5519dad148ef6f6f11dd7e78dbd
Andrew L <andjlindsay@gmail.com>
2023-03-04 12:52:36 -0800

    more impl

--- components/library_parser.py
+++ components/library_parser.py
@@ -1,15 +1,16 @@
 import logging
 import tidalapi
 from config.config import Config
-from domain.artist_behavior import ArtistBehavior
 
 
 class LibraryParser:
     def __init__(self, favorites: tidalapi.Favorites) -> None:
         self._favorites = favorites
-        self._all_tracks: "dict[int, tidalapi.Track]" = None
+        self._all_tracks: "dict[int, list[tidalapi.Track]]" = {}
         self._config = Config.instance().data
-        self._artist_max = self._config.get("max_per_artist", 100000)
+        self._artist_max = self._config.get("playlist_maximums").get(
+            "tracks_per_artist", 100000
+        )
 
     @property
     def all_tracks(self) -> list[tidalapi.Track]:
@@ -22,9 +23,12 @@ class LibraryParser:
     def track_ids(self) -> list[int]:
         return [t.id for t in self._all_tracks]
 
-    def artist_count(self, artist) -> int:
-        artist_tracks = [t for t in self._all_tracks if t.artist.id == artist.id]
+    def artist_count(self, artist_id: tidalapi.Artist) -> int:
+        artist_tracks = [t for t in self._all_tracks if t.artist.id == artist_id]
         return len(artist_tracks) if artist_tracks else 0
+
+    def artist_limit(self, artist_id: tidalapi.Artist) -> int:
+        return self._artist_max - self.artist_count(artist_id)
 
     def _parse(self):
         self._parse_tracks()
@@ -32,32 +36,59 @@ class LibraryParser:
         self._parse_artists()
 
     def _parse_tracks(self):
-        self._all_tracks = self._favorites.tracks()
+        # self._all_tracks = self._favorites.tracks()
+        all_tracks: list[tidalapi.Track] = self._favorites.tracks()
+        artist_ids = set([track.artist.id for track in all_tracks])
+        for artist_id in artist_ids:
+            tracks = [track for track in all_tracks if track.artist.id == artist_id]
+
+            if artist_id not in self._all_tracks:
+                self._all_tracks[artist_id] = []
+
+            self._all_tracks[artist_id].extend(
+                tracks[: self.artist_limit(artist_id) - 1]
+            )
 
     def _parse_albums(self):
         albums: list[tidalapi.album.Album] = self._favorites.albums()
+        albums_by_artist: "dict[int, list[tidalapi.Track]]" = {}
         for album in albums:
-            limit = self._artist_max - self.artist_count(album.artist)
-            tracks = album.tracks(limit=limit)
-            self._all_tracks.extend([t for t in tracks if t.id not in self.track_ids])
+            if album.artist.id not in albums_by_artist:
+                albums_by_artist[album.artist.id] = []
 
-    def _parse_artists(self, artist_behavior: ArtistBehavior):
-        data = Config.instance().data
-        artist_behavior = data.get("artist_behavior", ArtistBehavior.TOPX)
-        if artist_behavior == ArtistBehavior.NONE:
-            logging.info("Artist Behavior is set to NONE, skipping artist...")
+            albums_by_artist[album.artist.id].extend(album)
+
+        for artist_id, albums in albums_by_artist.items():
+            limit = self.artist_limit(artist_id)
+            if limit <= 0:
+                continue
+
+            for album in albums:
+                if limit <= 0:
+                    continue
+
+                tracks = album.tracks(limit=limit)
+                limit -= len(tracks)
+
+                if artist_id not in self._all_tracks:
+                    self._all_tracks[artist_id] = []
+
+                self._all_tracks[artist_id].extend(tracks)
+
+    def _parse_artists(self):
+        top_track_limit = self._config.get("favorite_artists").get("top_tracks", 10000)
+        if top_track_limit <= 0:
+            logging.info(
+                f"favorite_artists.top_tracks is <= 0, no tracks added for favorite artists."
+            )
+            return
 
         artists: list[tidalapi.artist.Artist] = self._favorites.artists()
-        artist_max = data.get("max_per_artist")
+        for artist in artists:
+            limit = min(self.artist_limit(artist.id), top_track_limit)
+            tracks = artist.get_top_tracks(limit)
 
-        if artist_behavior == ArtistBehavior.TOPX:
-            for artist in artists:
-                limit = artist_max - self.artist_count(artist)
-                self._all_tracks.extend(artist.get_top_tracks(limit=limit))
-        else:  # ArtistBehavior.ALL is the only remaining option
-            for artist in artists:
-                for album in artist.get_albums():
-                    limit = artist_max - self.artist_count(artist)
-                    if limit <= 0:
-                        break
-                    album.tracks(limit=limit)
+            if artist not in self._all_tracks:
+                self._all_tracks[artist] = []
+
+            self._all_tracks[artist].extend(tracks)
--- domain/artist_behavior.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class ArtistBehavior(Enum):
-    ALL = 1
-    TOPX = 2
-    NONE = 3
