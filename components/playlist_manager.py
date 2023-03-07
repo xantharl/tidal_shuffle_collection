c352961f0182cbcee9a6ccd0d738c3493917e03f
Andrew L <andjlindsay@gmail.com>
2023-03-06 22:20:59 -0800

    adding more routes

--- components/playlist_manager.py
+++ components/playlist_manager.py
@@ -1,3 +1,4 @@
+import json
 import tidalapi
 from components.library_parser import LibraryParser
 from connector.session_v2 import SessionV2
@@ -23,20 +24,57 @@ class PlaylistManager:
         }
 
         # basic_request(self, method, path, params=None, data=None, headers=None)
-        response = self._session.request_v2.basic_request(
+        return self._session.request_v2.basic_request(
             "PUT", f"my-collection/playlists/folders/create-playlist", params=params
         )
 
         return response
 
-    def get_lists_by_id(self, playlist_ids: list[str]):
-        pass
+    def get_lists_by_id(self, playlist_ids: list[str] = []):
+        # params = {
+        #     "includeOnly": "FAVORITE_PLAYLIST",
+        #     "offset": "0",
+        #     "limit": "50",
+        #     "order": "DATE",
+        #     "orderDirection": "DESC",
+        #     "countryCode": "US",
+        #     "locale": "en_US",
+        #     "deviceType": "BROWSER",
+        # }
+
+        params = {
+            "folderId": "root",
+            "offset": "0",
+            "limit": "50",
+            "order": "DATE",
+            "orderDirection": "DESC",
+            "countryCode": "US",
+            "locale": "en_US",
+            "deviceType": "BROWSE",
+        }
+
+        path = "my-collection/playlists/folders/flattened"
+        response = self._session.request_v2.basic_request("GET", path, params=params)
+        if response.status_code != 200:
+            raise Exception(f"Something went wrong! {response.reason}")
+
+        return json.loads(response.content)
 
     def delete_list(self, playlist_id: str = None, playlist_name: str = None):
         if not playlist_id and not playlist_name:
             raise Exception(
                 "PlaylistManager.delete_list: At least one of playlist_id or playlist_name must be provided"
             )
+
+        path = "my-collection/playlists/folders/remove"
+
+        params = {
+            "trns": f"trn:playlist:{playlist_id}",
+            "countryCode": "US",
+            "locale": "en_US",
+        }
+
+        return self._session.request_v2.basic_request("PUT", path, params=params)
 
     def update_from_parser(self, parser: LibraryParser):
         pass
--- components/tests/playlist_manager_test.py
+++ components/tests/playlist_manager_test.py
@@ -9,9 +9,14 @@ class PlaylistManagerTest(unittest.TestCase):
         self._auth = Auth("session_data.json")
         return PlaylistManager(session=self._auth.session)
 
+    def test_get_lists(self):
+        manager = self.setup()
+        lists = manager.get_lists_by_id()
+        print(1)
+
     def test_create_list(self):
         manager = self.setup()
-        list = manager.create_list()
+        # list = manager.create_list()
 
 
 if __name__ == "__main__":
